b9ac0e4ffc28300d88df0f9063480e486fbf3034
bijay <tmgbijay869@gmail.com>
2024-06-18 17:28:30 +0545

    first commit

--- sportsproject/settings.py
+++ sportsproject/settings.py
@@ -31,7 +31,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://bijayakumartamang.com.np',
     'https://sportprojects-production.up.railway.app/',
 ]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['sportprojects-production.up.railway.app','127.0.0.1', ]
 # Application definition
 
 INSTALLED_APPS = [
