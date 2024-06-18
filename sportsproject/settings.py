5cc36c247ff7709e4c7d45a08a9f5fd67d41a511
bijay <tmgbijay869@gmail.com>
2024-06-18 17:37:05 +0545

    first commit

--- sportsproject/settings.py
+++ sportsproject/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -128,7 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
-CKEDITOR_UPLOAD_PATH = 'uploads/ckeditor/'
+
 
 # Static files directories
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
