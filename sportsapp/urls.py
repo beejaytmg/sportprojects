f4e3ce84da820501b23d30e603a5bdb917f463e9
bijay <tmgbijay869@gmail.com>
2024-06-19 15:02:53 +0545

    first commit

--- sportsapp/urls.py
+++ sportsapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.league_list, name='league_list'),
     path('matches/<str:slug>/', views.league_matches, name='league_matches'),
     path('match/<str:slug>/', views.match_detail, name='match_detail'),
+    path('robots.txt', views.robots_txt, name='ads-txt'),
 ]
--- sportsapp/views.py
+++ sportsapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import League, Match, Team
-
+import os
+from django.http import HttpResponse, request
+from django.conf import settings
 def league_list(request):
     leagues = League.objects.all()
     return render(request, 'sports/league_list.html', {'leagues': leagues})
@@ -13,3 +15,11 @@ def league_matches(request, slug):
 def match_detail(request, slug):
     match = get_object_or_404(Match, slug=slug)
     return render(request, 'sports/match_detail.html', {'match': match})
+def robots_txt(request):
+    ads_file_path = os.path.join(settings.BASE_DIR, 'robots.txt')
+    try:
+        with open(ads_file_path, 'r') as file:
+            content = file.read()
+        return HttpResponse(content, content_type='text/plain')
+    except FileNotFoundError:
+        return HttpResponse('File not found.', status=404, content_type='text/plain')
